ad4c85abfc36afc0238bd14732a446b6f26b6ea9
Manjunatha Shety H <manjunatha.shetty_h@nokia.com>
2018-09-14 12:17:31 +0530

    Fix for #36

--- helm-app-operator/pkg/helm/engine.go
+++ helm-app-operator/pkg/helm/engine.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"strings"
 
+	"bytes"
+	"io"
+
 	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	yamlutil "k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/helm/pkg/chartutil"
 	"k8s.io/helm/pkg/proto/hapi/chart"
 	"k8s.io/helm/pkg/tiller/environment"
@@ -49,24 +54,40 @@ func (o *OwnerRefEngine) Render(chart *chart.Chart, values chartutil.Values) (ma
 }
 
 // addOwnerRefs adds the configured ownerRefs to a single rendered file
+// Adds the ownerrefs to all the documents in a YAML file
 func (o *OwnerRefEngine) addOwnerRefs(fileContents string) (string, error) {
-	parsed := chartutil.FromYaml(fileContents)
-	if errors, ok := parsed["Error"]; ok {
-		return "", fmt.Errorf("error parsing rendered template to add ownerrefs: %v", errors)
+
+	inputBuf := bytes.NewBuffer([]byte(fileContents))
+	decoder := yamlutil.NewYAMLOrJSONDecoder(inputBuf, 4096)
+
+	var outBuf bytes.Buffer
+	encoder := yaml.NewEncoder(&outBuf)
+
+	for {
+		var parsed = map[string]interface{}{}
+		err := decoder.Decode(&parsed)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return "", fmt.Errorf("error parsing rendered template to add ownerrefs: %v", err)
+		}
+
+		if len(parsed) == 0 {
+			return "", nil
+		}
+
+		unst, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&parsed)
+		if err != nil {
+			return "", err
+		}
+		unstructured := &unstructured.Unstructured{Object: unst}
+		unstructured.SetOwnerReferences(o.refs)
+
+		encoder.Encode(unstructured.Object)
 	}
 
-	// Empty input
-	if len(parsed) == 0 {
-		return "", nil
-	}
-
-	unst, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&parsed)
-	if err != nil {
-		return "", err
-	}
-	unstructured := &unstructured.Unstructured{Object: unst}
-	unstructured.SetOwnerReferences(o.refs)
-	return chartutil.ToYaml(unstructured.Object), nil
+	return outBuf.String(), nil
 }
 
 // NewOwnerRefEngine creates a new OwnerRef engine with a set of metav1.OwnerReferences to be added to assets
